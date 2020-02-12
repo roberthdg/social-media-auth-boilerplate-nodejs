3fa338e73d787cb1d3afbc333dc367258d302714
Roberth Gómez <roberthdgg@gmail.com>
2020-02-12 08:49:52 -0400

    GitHub auth boilerplate

---
+++ server.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const session     = require('express-session');
+const mongo       = require('mongodb').MongoClient;
+const passport    = require('passport');
+
+const app = express();
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'pug')
+
+mongo.connect(process.env.DATABASE, (err, db) => {
+    if(err) {
+        console.log('Database error: ' + err);
+    } else {
+        console.log('Successful database connection');
+      
+        app.use(session({
+          secret: process.env.SESSION_SECRET,
+          resave: true,
+          saveUninitialized: true,
+        }));
+        app.use(passport.initialize());
+        app.use(passport.session());
+      
+        function ensureAuthenticated(req, res, next) {
+          if (req.isAuthenticated()) {
+              return next();
+          }
+          res.redirect('/');
+        };
+
+        passport.serializeUser((user, done) => {
+          done(null, user.id);
+        });
+
+        passport.deserializeUser((id, done) => {
+            db.collection('socialusers').findOne(
+                {id: id},
+                (err, doc) => {
+                    done(null, doc);
+                }
+            );
+        });
+
+    
+        app.route('/auth/github')
+          .get(passport.authenticate('github'));
+      
+        app.route('/auth/github/callback')
+          .get(passport.authenticate('github', { failureRedirect: '/' }), (req,res) => {
+              res.redirect('/profile');
+          });
+        
+      
+        app.route('/')
+          .get((req, res) => {
+            res.render(process.cwd() + '/views/pug/index');
+          });
+
+        app.route('/profile')
+          .get(ensureAuthenticated, (req, res) => {
+               res.render(process.cwd() + '/views/pug/profile', {user: req.user});
+          });
+
+        app.route('/logout')
+          .get((req, res) => {
+              req.logout();
+              res.redirect('/');
+          });
+
+        app.use((req, res, next) => {
+          res.status(404)
+            .type('text')
+            .send('Not Found');
+        });
+      
+        app.listen(process.env.PORT || 3000, () => {
+          console.log("Listening on port " + process.env.PORT);
+        });  
+}});
