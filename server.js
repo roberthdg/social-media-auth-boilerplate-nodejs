9114bb98394b493a9d559ee983046b4d848881d6
Roberth Gómez <roberthdgg@gmail.com>
2020-02-12 09:28:14 -0400

    Added passport strategy

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser  = require('body-parser');
 const session     = require('express-session');
 const mongo       = require('mongodb').MongoClient;
 const passport    = require('passport');
+const GitHubStrategy = require('passport-github').Strategy
 
 const app = express();
 
@@ -47,6 +48,37 @@ mongo.connect(process.env.DATABASE, (err, db) => {
                 }
             );
         });
+
+              
+      passport.use(new GitHubStrategy({
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: 'https://glitch.com/auth/github/callback'
+      },
+        function(accessToken, refreshToken, profile, cb) {
+        db.collection('socialusers').findAndModify(
+              {id: profile.id},
+              {},
+              {$setOnInsert:{
+                id: profile.id,
+                name: profile.displayName || 'John Doe',
+                photo: profile.photos[0].value || '',
+                email: profile.emails[0].value || 'No public email',
+                created_on: new Date(),
+                provider: profile.provider || ''
+              },$set:{
+                last_login: new Date()
+              },$inc:{
+                login_count: 1
+              }},
+              {upsert:true, new: true},
+              (err, doc) => {
+                return cb(null, doc.value);
+              }
+            );
+          }
+    ));
+
 
     
         app.route('/auth/github')
